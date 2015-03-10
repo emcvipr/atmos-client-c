406bd817746d8c37c3e52a5d80fa7d783dd6b62d
Joe Conery <coneryj@yahoo.com>
2015-03-09 21:59:56 -0400

    modified to clone the rest-client-c into a dep directory first since it was an svn external before being migrated to git

--- autogen.sh
+++ autogen.sh
@@ -5,6 +5,7 @@
 ## automake and libtool do for us.
 ##
 
+RESTCLIENTREPO="https://github.com/emcvipr/rest-client-c"
 echo "Setting up libtool"
 libtoolize --force
 echo "Setting up aclocal"
@@ -15,6 +16,14 @@ echo "Generating config.h.in"
 autoheader
 echo "Generating Makefile.in"
 automake --add-missing
+echo "retrieving rest-client-c dependency repo"
+if [ -d "dep"  ]; then
+  echo "The rest client dep directory already exists will attempt to pull from github repo"
+  sh -c "cd dep && git pull origin master $RESTCLIENTREPO"
+else
+  echo "rest client dep directory does NOT exist. It will be created and checked out"
+  sh -c "mkdir dep && cd dep && git clone $RESTCLIENTREPO"
+fi
 echo "** runing autogen.sh in dep/rest-client-c"
 sh -c "cd dep/rest-client-c && ./autogen.sh"
 
